f3eaedebf69c603e2815eb7fd9d14722bc603635
김진호 <157768080+kim-jinho1@users.noreply.github.com>
2025-03-12 21:44:04 +0900

    [Bronze V] Title: 오늘 날짜, Time: 48 ms, Memory: 5544 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baekjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Today.ToString("yyyy-MM-dd"));  // 대소문자 유의
+        }
+    }
+}
