fdb27a9d1a52f980abbfdd24a78140bb3282b018
김진호 <157768080+kim-jinho1@users.noreply.github.com>
2025-03-17 20:25:46 +0900

    [Bronze V] Title: 자동완성, Time: 48 ms, Memory: 5372 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+       string input = Console.ReadLine();
+        if(input =="n" || input == "N")
+            Console.WriteLine("Naver D2");
+        else
+            Console.WriteLine("Naver Whale");
+    }
+}
