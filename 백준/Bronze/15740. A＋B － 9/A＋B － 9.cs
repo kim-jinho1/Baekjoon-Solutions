9d1961f1fe94325a277e25c5f6d5363024a3ad66
김진호 <157768080+kim-jinho1@users.noreply.github.com>
2025-04-03 00:21:20 +0900

    [Bronze V] Title: A+B - 9, Time: 52 ms, Memory: 5516 KB, Score: 40 point -BaekjoonHub

---
+++ 백준/Bronze/15740. A＋B － 9/A＋B － 9.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace BACKJOON
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+            Console.WriteLine(arr[0] + arr[1]);
+        }
+    }
+}
