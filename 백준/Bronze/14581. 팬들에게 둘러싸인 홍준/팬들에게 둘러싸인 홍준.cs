4c139fb89a168f1aa6441fc5f6fbef4e23d539be
김진호 <157768080+kim-jinho1@users.noreply.github.com>
2025-04-03 18:45:55 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 48 ms, Memory: 5372 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace BACKJOON
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string arr = Console.ReadLine();
+
+            Console.WriteLine($":fan::fan::fan:\n:fan::{arr}::fan:\n:fan::fan::fan:");
+        }
+    }
+}
