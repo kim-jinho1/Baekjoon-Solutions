ed00be207590e7a122400906bfef537a1a035c42
김진호 <157768080+kim-jinho1@users.noreply.github.com>
2025-04-09 22:38:53 +0900

    [Bronze V] Title: 홍익대학교, Time: 48 ms, Memory: 5476 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace BACKJOON
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine(a - 1946);
+        }
+    }
+}
