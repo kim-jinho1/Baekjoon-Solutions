6dcb864182945f3badc15c2fb942fc9a0a5e4d24
김진호 <157768080+kim-jinho1@users.noreply.github.com>
2025-04-10 22:45:52 +0900

    [Unrated] Title: 엔드게임 스포일러, Time: 44 ms, Memory: 5324 KB -BaekjoonHub

---
+++ 백준/Unrated/17295. 엔드게임 스포일러/엔드게임 스포일러.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace BACKJOON
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Avengers: Endgame");
+        }
+    }
+}
