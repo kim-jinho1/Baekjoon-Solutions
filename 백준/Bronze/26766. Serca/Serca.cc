2118359d9ea566dddf96acef955f684afa49c1c8
김진호 <157768080+kim-jinho1@users.noreply.github.com>
2025-04-22 22:57:21 +0900

    [Bronze V] Title: Serca, Time: 116 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int N;
+
+	cin >> N;
+	while (N--) {
+		cout << " @@@   @@@ " << '\n';
+		cout << "@   @ @   @" << '\n';
+		cout << "@    @    @" << '\n';
+		cout << "@         @" << '\n';
+		cout << " @       @ " << '\n';
+		cout << "  @     @  " << '\n';
+		cout << "   @   @   " << '\n';
+		cout << "    @ @    " << '\n';
+		cout << "     @     " << '\n';
+	}
+}
