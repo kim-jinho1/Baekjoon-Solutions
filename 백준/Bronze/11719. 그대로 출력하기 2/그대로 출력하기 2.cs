36409da663108d00f62c5f0c5d65d50be8bfbc2c
김진호 <157768080+kim-jinho1@users.noreply.github.com>
2025-04-27 22:58:35 +0900

    [Bronze III] Title: 그대로 출력하기 2, Time: 52 ms, Memory: 5376 KB -BaekjoonHub

---
+++ 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace BACKJOON
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            while (a != 100)
+            {
+                a++;
+                Console.WriteLine(Console.ReadLine());
+            }
+        }
+    }
+}
