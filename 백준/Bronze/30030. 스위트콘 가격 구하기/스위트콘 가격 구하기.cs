0bff3c636a50f425613193192e1d7de5f9c57431
김진호 <157768080+kim-jinho1@users.noreply.github.com>
2025-05-02 22:32:57 +0900

    [Bronze V] Title: 스위트콘 가격 구하기, Time: 48 ms, Memory: 5476 KB -BaekjoonHub

---
+++ 백준/Bronze/30030. 스위트콘 가격 구하기/스위트콘 가격 구하기.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace BACKJOON
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine((n / 11) * 10);
+        }
+    }
+}
