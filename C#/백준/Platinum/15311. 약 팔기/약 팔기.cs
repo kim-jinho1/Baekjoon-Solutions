ac318807a07255aee118c4a07ef90ab19a8d6722
김진호 <157768080+kim-jinho1@users.noreply.github.com>
2025-05-12 23:21:48 +0900

    [Platinum V] Title: 약 팔기, Time: 96 ms, Memory: 5320 KB -BaekjoonHub

---
+++ C#/백준/Platinum/15311. 약 팔기/약 팔기.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace BACKJOON
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("2000");
+            for (int i = 0; i < 1000; ++i)
+                Console.Write("1 ");
+            for (int i = 0; i < 1000; ++i)
+                Console.Write("1000 ");
+        }
+    }
+}
