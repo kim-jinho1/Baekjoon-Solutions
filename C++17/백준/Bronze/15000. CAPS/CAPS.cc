6ba29fe30b3d4225d46fe06201fc1a5fb32e39f5
김진호 <157768080+kim-jinho1@users.noreply.github.com>
2025-05-25 22:39:12 +0900

    [Bronze V] Title: CAPS, Time: 32 ms, Memory: 4916 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/15000. CAPS/CAPS.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string s;
+    cin >> s;
+    for (char &c : s) {
+        c = toupper(c);
+    }
+    cout << s << '\n';
+}
