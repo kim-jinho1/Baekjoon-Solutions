85b02d88ec76ead2e79a0bc9d09351a95b6c7a1a
김진호 <157768080+kim-jinho1@users.noreply.github.com>
2025-06-13 20:39:31 +0900

    [Bronze V] Title: Correct, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/26307. Correct/Correct.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int hh, mm; cin >> hh >> mm;
+
+  int startTime = 9 * 60,
+      submitTime = hh * 60 + mm;
+
+  int timeConsumed = submitTime - startTime;
+
+  cout << timeConsumed << "\n";
+}
