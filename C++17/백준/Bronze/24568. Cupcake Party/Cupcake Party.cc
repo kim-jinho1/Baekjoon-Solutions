015cfbe71739cefe679329078c8c0b83d622ea91
김진호 <157768080+kim-jinho1@users.noreply.github.com>
2025-06-27 21:21:19 +0900

    [Bronze V] Title: Cupcake Party, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/24568. Cupcake Party/Cupcake Party.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int r, s;
+    cin >> r >> s;
+
+    int num1 = r * 8 + s * 3;
+    int num2 = num1 - 28;
+
+    cout << num2 << endl;
+}
