4d0cf8bf782b3ae395fc657a1c551db319bb93f5
김진호 <157768080+kim-jinho1@users.noreply.github.com>
2025-07-01 10:15:04 +0900

    [Bronze V] Title: Mathematics, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/26545. Mathematics/Mathematics.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, sum = 0, num;
+    cin >> n;
+
+    for (int i = 0; i < n; ++i) {
+        cin >> num;
+        sum += num;
+    }
+
+    cout << sum << endl;
+}
