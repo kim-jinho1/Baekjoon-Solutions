011fe2d34fa183cd6d1d5e43cf75680333c66162
김진호 <157768080+kim-jinho1@users.noreply.github.com>
2025-07-06 12:36:43 +0900

    [Bronze V] Title: Hello Judge, Time: 56 ms, Memory: 5760 KB -BaekjoonHub

---
+++ C#/백준/Bronze/9316. Hello Judge/Hello Judge.cs
@@ -0,0 +1,12 @@
+namespace BACKJOON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            for (int i = 0; i < a; i++)
+                Console.WriteLine($"Hello World, Judge {i + 1}!");
+        }
+    }
+}
