63c16af76defdc763eed728474d9ea4f3c69934b
김진호 <157768080+kim-jinho1@users.noreply.github.com>
2025-07-19 20:18:42 +0900

    [Bronze V] Title: An Easy-Peasy Problem, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/30214. An Easy－Peasy Problem/An Easy－Peasy Problem.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int s1, s2;
+    cin >> s1 >> s2;
+    if (s1 * 2 >= s2)
+        cout << "E" << endl;
+    else
+        cout << "H" << endl;
+}
