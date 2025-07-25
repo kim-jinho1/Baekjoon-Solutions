cc103fba263153e60d05afd5789b7507cb4a386d
김진호 <157768080+kim-jinho1@users.noreply.github.com>
2025-07-25 22:10:44 +0900

    [Bronze V] Title: Micromasters, Time: 48 ms, Memory: 5464 KB -BaekjoonHub

---
+++ C#/백준/Bronze/33178. Micromasters/Micromasters.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace BACKJOON
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int freeCourses = n / 10;
+
+            Console.WriteLine(freeCourses);
+        }
+    }
+}
