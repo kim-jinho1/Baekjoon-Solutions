143cbc204b8e2771539490078a81329c264ed74e
김진호 <157768080+kim-jinho1@users.noreply.github.com>
2025-07-29 21:47:35 +0900

    [Bronze IV] Title: Celebrity jeopardy, Time: 48 ms, Memory: 5364 KB -BaekjoonHub

---
+++ C#/백준/Bronze/3765. Celebrity jeopardy/Celebrity jeopardy.cs
@@ -0,0 +1,11 @@
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string line;
+        while ((line = Console.ReadLine()) != null)
+        {
+            Console.WriteLine(line);
+        }
+    }
+}
