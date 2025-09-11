d1b102a4e2a46300abbd5a5b867b46af9b052454
김진호 <157768080+kim-jinho1@users.noreply.github.com>
2025-09-11 19:45:52 +0900

    [Bronze III] Title: 얼마?, Time: 28 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/9325. 얼마？/얼마？.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int s, n;
+        cin >> s >> n;
+        while (n--) {
+            int q, p;
+            cin >> q >> p;
+            s += q * p;
+        }
+        cout << s << "\n";
+    }
+}
