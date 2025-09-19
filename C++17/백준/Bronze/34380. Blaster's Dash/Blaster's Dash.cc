ceac7cea9f7976056df43bbf4775fa8642871b6b
김진호 <157768080+kim-jinho1@users.noreply.github.com>
2025-09-19 09:38:20 +0900

    [Bronze V] Title: Blaster's Dash, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/34380. Blaster's Dash/Blaster's Dash.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int m, v, n, k;
+    cin >> m >> v;
+    cin >> n;
+    cin >> k;
+
+    cout << 2 * (n + 20) << endl;
+    return 0;
+}
