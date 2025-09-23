bb6fd13e50a17fff7369934627c9781344eed591
김진호 <157768080+kim-jinho1@users.noreply.github.com>
2025-09-23 18:55:41 +0900

    [Bronze IV] Title: Triangles, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/7595. Triangles/Triangles.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    while (cin >> n && n != 0)
+        for (int i = 1; i <= n; i++)
+            cout << string(i, '*') << '\n';
+}
