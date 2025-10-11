e1f4c0bff24f417b5d339b510f2fc92bd34e4f7b
김진호 <157768080+kim-jinho1@users.noreply.github.com>
2025-10-11 22:09:05 +0900

    [Bronze IV] Title: Judging Moose, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/15025. Judging Moose/Judging Moose.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int l, r;
+    cin >> l >> r;
+    if (l == 0 && r == 0)
+        cout << "Not a moose";
+    else if (l == r)
+        cout << "Even " << l + r;
+    else
+        cout << "Odd " << max(l, r) * 2;
+}
