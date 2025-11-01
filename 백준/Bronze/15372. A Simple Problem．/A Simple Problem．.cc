a8ead491544e2cfcdc17ea026f234113c431bcd1
김진호 <157768080+kim-jinho1@users.noreply.github.com>
2025-11-01 21:46:03 +0900

    [Bronze IV] Title: A Simple Problem., Time: 36 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15372. A Simple Problem．/A Simple Problem．.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        cout << 1LL * n * n << '\n';
+    }
+}
