46dc0b412735d9d806b35b93956e64177d449ccc
김진호 <157768080+kim-jinho1@users.noreply.github.com>
2025-11-09 18:11:55 +0900

    [Bronze V] Title: 역사를 걸으면 동국이 보이고, Time: 48 ms, Memory: 5492 KB -BaekjoonHub

---
+++ 백준/Bronze/34721. 역사를 걸으면 동국이 보이고/역사를 걸으면 동국이 보이고.cs
@@ -0,0 +1,12 @@
+namespace BACKJOON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+                Console.WriteLine("I love DGU");
+        }
+    }
+}
