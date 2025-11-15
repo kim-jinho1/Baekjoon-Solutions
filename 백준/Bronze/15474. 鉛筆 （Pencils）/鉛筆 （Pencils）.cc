009e0ef54b9e517d3be655963ec22c9afb3199ec
김진호 <157768080+kim-jinho1@users.noreply.github.com>
2025-11-15 22:01:19 +0900

    [Bronze IV] Title: 鉛筆 (Pencils), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15474. 鉛筆 （Pencils）/鉛筆 （Pencils）.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n, a, b, c, d;
+    cin >> n >> a >> b >> c >> d;
+    int x = ((n + a - 1) / a) * b;
+    int y = ((n + c - 1) / c) * d;
+    cout << (x < y ? x : y);
+}
