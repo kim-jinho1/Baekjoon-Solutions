3d768fb3db2335ac9d04f3f34c5fdfc680812582
김진호 <157768080+kim-jinho1@users.noreply.github.com>
2025-11-21 22:26:47 +0900

    [Bronze IV] Title: Counting Clauses, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/17903. Counting Clauses/Counting Clauses.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int m, n;
+    cin >> m >> n;
+    if (m >= 8) 
+        cout << "satisfactory";
+    else
+        cout << "unsatisfactory";
+}
